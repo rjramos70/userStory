266092cf6a636f8e2312611a8f1845909246288e
Renato Ramos <renatoramos@Renatos-MacBook-Pro.local>
2016-10-01 00:42:48 -0300

    Adding all the files for the first time

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	"database": "mongodb://root:abc123@ds035776.mlab.com:35776/rjramos70db",
+	"port": process.env.PORT || 3000,
+	//"secretKey": "YourSecretKey"
+	"secretKey": "BananaComCanela"
+}
